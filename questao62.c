db06cb6164672d0f3459ac370bec310700f3e7b0
Beatriz Macêdo <mbiaamacedo@gmail.com>
2025-11-14 23:22:35 -0300

    Create questao62.c

---
+++ questao62.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int A[4][4], D[4], i, j, soma = 0;
+
+    printf("--- matriz A ---\n");
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("Digite A[%d][%d]: ", i, j);
+            scanf("%d", &A[i][j]);
+        }
+    }
+    for (i = 0; i < 4; i++) {
+        j = 3 - i;
+        soma += A[i][j]; 
+        D[i] = A[i][j]; 
+    }
+    printf("soma da diagonal secundaria: %d\n", soma);
+    printf("vetor D :\n");
+    for (i = 0; i < 4; i++) {
+        printf("%d ", D[i]);
+    }
+    printf("\n");
+    return 0;
+}
